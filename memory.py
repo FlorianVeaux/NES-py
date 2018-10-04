a02440eed501f2678888e81e82726637a191afd0
Nicolas Couvrat <nicolas.couvrat@mines-paristech.fr>
2018-10-04 22:59:07 +0900

    add memory, edit gitignore for vim

---
+++ memory.py
@@ -0,0 +1,18 @@
+# must implement
+# reset()
+# fetch (address) return value
+# storei (address, value) return old_value
+
+
+import numpy as np
+
+MEM_SIZE = 65536
+
+class Memory(object):
+    def __init__(self):
+        self._memory = np.zeros(MEM_SIZE, dtype='uint8')
+
+    def reset():
+        self.
+        
+    
